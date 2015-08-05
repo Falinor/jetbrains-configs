593b7d3023fc362f99cabfa846e7081828ffe9ce
Gueugnaut Andrea <gueugn_a@epita.fr>
2015-08-06 00:56:07 +0200

    Update shell script installer

--- install.sh
+++ install.sh
@@ -2,31 +2,25 @@
 
 # TODO: INSTALL MYSQL AND POSTGRESQL
 
+sudo apt-get update
+
 # Oh my zsh
-#sudo apt-get install zsh
-#chsh -s /usr/bin/zsh
-#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-zsh --version >> setup.log
+sudo apt-get install zsh
+chsh -s /usr/bin/zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+zsh --version > setup.log
 
 # Java
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo apt-get update
-#sudo apt-get install oracle-java8-installer
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 java -version >> setup.log
 
 ### Install hidden files and directories ###
 
 # Composer (for PHP and Laravel framework)
 curl -sS https://getcomposer.org/installer | php
-cd /usr/bin && ln -s $OLDPWD/composer.phar composer
-
-# Git
-echo 'Installing git...' >> setup.log
-#sudo apt-get install git && echo 'Installed git' >> setup.log || exit 1
-
-# Clone configs repo
-echo 'Cloning configs repo from bitbucket...' >> setup.log
-#git clone https://Inadvertance@bitbucket.org/Inadvertance/configs.git
+cd /usr/bin && sudo ln -s $OLDPWD/composer.phar composer
 
 cd $HOME && echo "Going to $HOME..." >> setup.log
 # Loop over files
@@ -40,13 +34,17 @@ for file in $(ls -a $OLDPWD); do
   esac
 done
 
-echo "----- TODO -----"
+echo "----- TODO -----\n"
+
 echo "Install IntelliJIdea"
 echo "Install Gimp"
-echo "Reload fonts to take ~/.fonts/Monaco into account"
-echo "Setup ssh"
-echo "----- Optionals -----"
+echo "Download Monaco font and reload ~/.fonts/Monaco into account"
+echo "Setup ssh\n"
+
+echo "----- Optionals -----\n"
+
 echo "Retrieve lessons and projects from bitbucket repositories"
 echo "Install i3 (if necessary)"
 echo "Get an image background"
-echo "Install Brackets"
+
+echo "Install Brackets\n"
